08f02b75c338e52026ed23079dee5385dcb0f6e1
Daniyal Naeem <daniyalnaeem5@gmail.com>
2023-06-01 10:41:29 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+View,
+StyleSheet,
+Text
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+
+  return (
+   <View style={{alignItems: 'center'}}>
+   <Text>Litigation App</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
