6795c1e25eccb7a311301335f3d19c2f842fbd2f
Daniyal Naeem <daniyalnaeem5@gmail.com>
2023-06-05 14:31:55 +0500

    updated app.js

--- App.tsx
+++ App.tsx
@@ -1,25 +1,28 @@
+import React, {useEffect} from 'react';
+import RootNavigation from './src/navigation/index';
+import {Provider} from 'react-redux';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+import reduxStore from './src/redux';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-View,
-StyleSheet,
-Text
-} from 'react-native';
+export const reduxPersistStore = persistStore(reduxStore);
 
 
+const App = () => {
 
-function App(): JSX.Element {
+  useEffect(() => {
+    EStyleSheet.build();
+  }, []);
+
 
   return (
-   <View style={{alignItems: 'center'}}>
-   <Text>Litigation App</Text>
-   </View>
-  );
+    <Provider store={reduxStore}>
+      <PersistGate persistor={reduxPersistStore}>
+        <RootNavigation />
+      </PersistGate>
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
- 
-});
 
 export default App;
