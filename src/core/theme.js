891d5d9f754dc029b8b8b37e10270ba6dff1fbaa
Daniyal Naeem <daniyalnaeem5@gmail.com>
2023-07-03 16:45:47 +0500

    updated colors

--- src/core/theme.js
+++ src/core/theme.js
@@ -6,7 +6,7 @@ export const theme = {
     ...DefaultTheme.colors,
     bgColor:'#823D3F',
     text: '#000000',
-    primary: '#257A52',
+    primary: '#823D3F',
     secondary: '#414757',
     error: '#f13a59',
   },
