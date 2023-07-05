936e25f62e88ee8482fe26188272638bb24e945f
Daniyal Naeem <daniyalnaeem5@gmail.com>
2023-07-05 16:23:02 +0500

    updated home navigation

--- navigation/HomeNavigation.js
+++ navigation/HomeNavigation.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createStackNavigator } from '@react-navigation/stack'
 import MainScreen from '../src/screens/MainScreen';
-import IndustrialUnits from '../src/screens/IndustrialUnits';
+import CourtCases from '../src/screens/CourtCases';
 import ChangePasswordScreen from '../src/components/ChangePassword';
+import CasesList from '../src/screens/CasesList' 
 
 
 const Tab = createMaterialTopTabNavigator();
@@ -17,8 +18,9 @@ const Stack = createStackNavigator()
     screenOptions={{headerShown: false}}
     >
       <Stack.Screen name="Main Screen" component={MainScreen} />
-      <Stack.Screen name="Industrial Units" component={IndustrialUnits} />
+      <Stack.Screen name="Court Cases" component={CourtCases} />
       <Stack.Screen name="Change Password" component={ChangePasswordScreen} />
+      <Stack.Screen name="Cases List" component={CasesList} />
     </Stack.Navigator>
 
   )
